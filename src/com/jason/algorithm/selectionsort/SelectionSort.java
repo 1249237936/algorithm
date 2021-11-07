27335c464bdf60d75883b3f48a202a9c9fcb42ea
Jason <15521215849@163.com>
2021-11-07 12:17:34 +0800

    冒泡排序带注释

--- src/com/jason/algorithm/selectionsort/SelectionSort.java
+++ src/com/jason/algorithm/selectionsort/SelectionSort.java
@@ -2,6 +2,13 @@ package com.jason.algorithm.selectionsort;
 
 public class SelectionSort {
 
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+
     public static void selectSort(int[] arr) {
         if (arr == null || arr.length < 2) return;
 
@@ -16,10 +23,22 @@ public class SelectionSort {
 
     }
 
-    public static void swap(int[] arr, int i, int j) {
-        int temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) return;
+        //0 ~ n - 1
+        //0 ~ n - 2
+        //0 ~ n - 3
+        //0 ~ end
+        int N = arr.length;
+        for (int end = N - 1; end >= 0; end--) {
+            // 0 ~ end 干一坨事情
+            // 0 1  1 2  2 3    end - 1 end
+            for (int second = 1; second <= end; second++) {
+                if (arr[second - 1] > arr[second]) {
+                    swap(arr, second - 1, second);
+                }
+            }
+        }
     }
 
     public static void printSort(int[] arr) {
@@ -32,7 +51,8 @@ public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {7, 1, 3, 5, 1, 6, 8, 1, 3, 5, 7, 5, 6};
         printSort(arr);
-        selectSort(arr);
+        //selectSort(arr);
+        bubbleSort(arr);
         printSort(arr);
     }
 }
